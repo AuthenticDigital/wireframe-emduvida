fc747fa639e47dc797be92defdd933059ab3f297
Authentic It Solutions <authenticagencia@gmail.com>
2015-03-26 20:57:54 -0300

    Conteudo finalizado

---
+++ conteudo.php
@@ -0,0 +1,307 @@
+
+<article class="conteudo">
+
+
+    <article class="exibirConteudo">
+        <h1 class="tituloPadrao"><i class="fa fa-star"></i>Mais votados</h1>
+
+        <a href="">
+            <figure class="figureProd destaque">
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C <span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque destaquestar" ></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal" >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+
+                <div class="imgDestaque" id="star1" ></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal" >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star2"></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star3"></div>
+            </figure> 
+        </a>
+
+
+        <a href="">    
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star4"></div>
+            </figure> 
+        </a>
+
+        <a href="">
+
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star5" ></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star6"></div>
+            </figure> 
+        </a>
+
+
+    </article>
+    
+    
+    <article class="exibirConteudo">
+        <h1 class="tituloPadrao"><i class="fa fa fa-eye"></i>Mais vistos</h1>
+
+        <a href="">
+            <figure class="figureProd destaque">
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C <span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque destaquestar" ></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal" >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+
+                <div class="imgDestaque" id="star1" ></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal" >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star2"></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star3"></div>
+            </figure> 
+        </a>
+
+
+        <a href="">    
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star4"></div>
+            </figure> 
+        </a>
+
+        <a href="">
+
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star5" ></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star6"></div>
+            </figure> 
+        </a>
+
+
+    </article>
+    
+    
+    <article class="exibirConteudo">
+        <h1 class="tituloPadrao"><i class="fa fa-thumbs-o-down"></i>Negativas</h1>
+
+        
+        <a href="">
+            <figure class="figureProd normal" >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+
+                <div class="imgDestaque" id="star1" ></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal" >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star2"></div>
+            </figure> 
+        </a>
+
+        <a href="">
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star3"></div>
+            </figure> 
+        </a>
+
+
+        <a href="">    
+            <figure class="figureProd normal " >
+
+                <figcaption class="legendaProduto"><span>Iphone 5 C<span class="txtCanto"><i class="fa fa-star"></i> 8,9</span> </span></figcaption>
+                <div class="imgDestaque" id="star4"></div>
+            </figure> 
+        </a>
+
+      
+
+    </article>
+</article>
+
+
+
+
+
+<!--            <h1> Mais Votados</h1>
+            
+            <section class="campo1">
+                <section id="segura">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                </section>
+                
+                <section class="segura1">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                    </section>
+                <section class="segura1">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                <section class="segura1">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                <section class="segura2">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                <section class="segura2">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                </section>
+                <section class="segura2">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                </section>
+            </section>
+            
+            <h1> Mais Visualizados</h1>
+            
+            
+            <section class="campo1">
+                
+                <section class="segura1">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                </section>
+                
+                <section class="segura1">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                    </section>
+                <section class="segura1">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                
+                
+                <section id="segura4">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                <section class="segura3">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                <section class="segura3">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                </section>
+                <section class="segura3">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                </section>
+            </section>
+            
+            
+            <h1> Recentes </h1>
+            
+            
+            <section id="campo2">
+                
+                <section class="seguraR">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                </section>
+                
+                <section class="seguraR">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                    </section>
+                <section class="seguraR">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                    <section class="seguraR">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                    </section>
+                <section class="seguraR">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                
+             </section>   
+                
+                
+                <section id="campo3">
+                    <h1> Negativos</h1>
+                
+                <section class="seguraP">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                </section>
+                
+                <section class="seguraP">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                    </section>
+                <section class="seguraP">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                    <section class="seguraP">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                    </section>
+                <section class="seguraP">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+                    <section class="seguraP">
+                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
+                    
+                </section>
+-->
--- index.php
+++ index.php
@@ -25,138 +25,23 @@
         include_once "./header.php";
         
         
+        ?>
+        
+        <section id="corpo">
+        
+        <?php
+        //Incluindo a página de conteudo inicial do site:
+        
+        include_once './conteudo.php';
         
         ?>
         
-        <section id="conteudo">
-            <h1> Mais Votados</h1>
-            
-            <section class="campo1">
-                <section id="segura">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                </section>
-                
-                <section class="segura1">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                    </section>
-                <section class="segura1">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                <section class="segura1">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                <section class="segura2">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                <section class="segura2">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                </section>
-                <section class="segura2">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                </section>
-            </section>
-            
-            <h1> Mais Visualizados</h1>
-            
-            
-            <section class="campo1">
-                
-                <section class="segura1">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                </section>
-                
-                <section class="segura1">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                    </section>
-                <section class="segura1">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                
-                
-                <section id="segura4">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                <section class="segura3">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                <section class="segura3">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                </section>
-                <section class="segura3">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                </section>
-            </section>
-            
-            
-            <h1> Recentes </h1>
-            
-            
-            <section id="campo2">
-                
-                <section class="seguraR">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                </section>
-                
-                <section class="seguraR">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                    </section>
-                <section class="seguraR">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                    <section class="seguraR">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                    </section>
-                <section class="seguraR">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                
-             </section>   
-                
-                
-                <section id="campo3">
-                    <h1> Negativos</h1>
-                
-                <section class="seguraP">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                </section>
-                
-                <section class="seguraP">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                    </section>
-                <section class="seguraP">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                    <section class="seguraP">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                    </section>
-                <section class="seguraP">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                    <section class="seguraP">
-                    <a href="pag_resenha.html" class="es">Nome da Resenha</a>
-                    
-                </section>
-                
-                </section>
+        
          
         </section>
-            <footer id="rodape">
+        
+        
+<!--            <footer id="rodape">
                 <img src="img/icon_gg.png" alt="icon_gg" id="logoEmp">
                 <a href="#" ><img src="img/icon_fb.png" alt="icon_fb" class="icones"></a>
                 <a href="#" ><img src="img/icon_tw.png" alt="icon_tw" class="icones1"></a>
@@ -164,6 +49,6 @@
                 <a href="#" class="pp"> Política de Privacidade</a>
                 <a href="#" class="pp"> Termos de Uso</a>
                 <p id="mark">All Rights Reserved - Todos os Direitos Reservados<br/><br/>Em Dúvida 2015</p>
-            </footer>
+            </footer>-->
     </body>
 </html>
