8c30910f78c928d85900285d8fa3516132604b58
Matheus <Matheus@Matheus>
2015-03-27 14:16:20 -0300

    Formulário Login

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Em Dúvida</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+
+    </head>
+
+<body>
+    <form method="post" action="" id="FormLogin">
+                <label for="Email">
+                    <span>Email: </span><input type="text" class="formLogin" name="Email">
+                </label>
+                <label for="Senha">
+                    <span>Senha: </span> <input  class="formLogin" type="text" name="Senha"/>
+                </label>
+    </form>
+</body>
+</html>
