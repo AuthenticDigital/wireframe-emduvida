66ded0b662307f5a131e1e8263045cfd6a6a36ae
Matheus <Matheus@Matheus>
2015-03-27 16:08:23 -0300

    tabela produtos

---
+++ tplProdutos/Tabelaprodutos.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Em Dúvida</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+
+    </head>
+    <body>
+        <form method="post" action="" id="FormProduto">
+            <input type="text" class="" name="pesquisa" placeholder="Pesquisar">
+            <input type="submit" class="" name="pesquisar" value="Pesquisar">
+        </form>
+        <br><br>
+        <table id="tabelaspec">
+    <caption>Produtos</caption>
+
+    <tr><td ><span>Nome Produto:</span><input type="text" class="" name="NomeProduto"></td></tr>
+    <tr><td ><span>Código do Produto:</span><input type="text" class="" name="CódigoProduto"></td></tr>
+    <tr><td>
+    <input  class="" type="submit" value="Excluir" name="Excluir"/>
+    <input  class="" type="submit" value="Alterar" name="alterar"/>
+    </td></tr>
+        </table>
+    </body>
+</html>    
+
+
+
+
