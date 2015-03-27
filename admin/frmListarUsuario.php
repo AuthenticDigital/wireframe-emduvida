ff27bcedd95654323d7741041af4957731875035
Matheus <Matheus@Matheus>
2015-03-27 16:24:37 -0300

    listagem dos usuários

---
+++ admin/frmListarUsuario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Em Dúvida</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+
+    </head>
+    <body>
+
+        <form method="post" action="" id="">
+                <label for="NomedoUsuario">
+                    <span>Nome do Usuário: </span><input type="text" class="" name="nomeUsu">
+                </label>
+                <br><br>
+                <input  class="formContato" type="submit" value="Listar" name="listausu"/>
+          
+        
+        
+        </form>
+        
+    </body>    
+</html>    
